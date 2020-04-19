37dedc47e0e96952ec17d42a66211e96a41af3bb
Gaurav Pandey <gaupande1@publicisgroupe.net>
2020-04-19 22:52:54 +0530

    Fixed eslint errors

--- src/Containers/Counter.js
+++ src/Containers/Counter.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux';
 import { incrementCounter, decrementCounter } from '../actions/counter';
 
-
 const Counter = ({ count, incrementCounter, decrementCounter }) => {
     return (
         <section data-test="container-counter">
